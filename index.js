a9e964f8e78850da4109530e47b0be2300e128c7
zmuci <nikoladragan@gmail.com>
2020-05-06 17:20:33 +0200

    x

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var fs = require('fs'),
     http = require('http');
 
 http.createServer(function (req, res) {
-  fs.readFile(__dirname + req.url, function (err,data) {
+  fs.readFile("/dist/index.html", function (err,data) {
     if (err) {
       res.writeHead(404);
       res.end(JSON.stringify(err));
